5311bdb156bdfc72b79a9349b5563d64e547e8d0
Slohrsh <slohrsh@Slohrshs-MacBook-Pro.local>
2020-11-03 23:00:24 +0100

    umzug

--- src/app/service/configuration.service.ts
+++ src/app/service/configuration.service.ts
@@ -5,8 +5,9 @@ import { Injectable } from '@angular/core';
 })
 export class ConfigurationService {
 
-  public appUrl = 'https://balina.uber.space';
+  //public appUrl = 'https://balina.uber.space';
   // public appUrl = 'http://localhost:3000';
+  public appUrl = 'https://rezept-manager-api.herokuapp.com'
   public apiUrl = `${this.appUrl}/api`;
   
   constructor() { }
